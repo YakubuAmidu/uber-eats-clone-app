2f1f1c9803432e2495c3a7681138300069940a7a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-07-11 23:06:11 -0700

    Initial commit

--- components/Categories.js
+++ components/Categories.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react';
 
 const items = [
@@ -30,8 +30,27 @@ const items = [
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={{
+      marginTop: 5,
+      backgroundColor: '#fff',
+      paddingVertical: 10,
+      paddingLeft: 20
+    }}>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {items.map((item, index) => (
+           <View key={index} style={{ alignItems: 'center', marginRight: 30 }}>
+           <Image
+            source={item.image}
+            style={{ 
+             width: 50,
+             height: 40,
+             resizeMode: 'contain',
+            }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: '900' }}>{items[0].text}</Text>
+           </View>
+      ))}
+    </ScrollView>
     </View>
   )
 }
