03b7b78052883f52ecd34f506d67a86bc667082a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-08-03 01:28:27 -0700

    Initial commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,30 @@
 import { View, SafeAreaView, ScrollView } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import HeaderTabs from '../components/HeaderTabs';
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
 import  RestaurantItems, { localRestaurants, } from '../components/RestaurantItems';
+import { YELP_API_KEY, yelpUrl } from '../yelpApi/yelpApi';
 
 export default function Home() {
   const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+
+  const apiOptions = {
+    headers: {
+      Authorization: `Bearer ${YELP_API_KEY}`,
+    },
+  }
+
+  return fetch(yelpUrl, apiOptions).then((res) => res.json()).then((json) => setRestaurantData(json.businesses))
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
+
   return (
     <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
       <View style={{ backgroundColor: 'white', padding: 15 }}>
