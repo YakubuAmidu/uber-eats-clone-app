a66aa4c8224474863c2f9a2c327b24a534ee8fd6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-08-03 00:32:37 -0700

    Initial commit

--- screens/Home.js
+++ screens/Home.js
@@ -4,9 +4,10 @@ import React from 'react';
 import HeaderTabs from '../components/HeaderTabs';
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
-import RestaurantItems from '../components/RestaurantItems';
+import  RestaurantItems, { localRestaurants, } from '../components/RestaurantItems';
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
   return (
     <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
       <View style={{ backgroundColor: 'white', padding: 15 }}>
@@ -15,7 +16,7 @@ export default function Home() {
       </View>
       <ScrollView showHorizontalIndicator={false}>
       <Categories />
-      <RestaurantItems />
+      <RestaurantItems restaurantData={restaurantData} />
       </ScrollView>
     </SafeAreaView>
   )
