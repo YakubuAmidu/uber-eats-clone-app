4789f81ee2f8ca17056ee29ce71a52a29c971183
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-08-07 16:54:59 -0700

    Initial commit

--- navigation.js
+++ navigation.js
@@ -3,8 +3,14 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Provider as ReduxProvider } from 'react-redux';
+
+import configureStore from './redux/store';
+
 import Home from './screens/Home';
 import RestaurantDetail from './screens/RestaurantDetail';
+
+const store = configureStore();
 
 export default function RootNavigation(){
   const Stack = createNativeStackNavigator();
@@ -13,11 +19,13 @@ export default function RootNavigation(){
     headerShown: false
   }
  return (
-  <NavigationContainer >
+  <ReduxProvider store={store}>
+    <NavigationContainer >
     <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
     </Stack.Navigator>
   </NavigationContainer>
+  </ReduxProvider>
  )
 }
--- redux/reducers/cartReducer.js
+++ redux/reducers/cartReducer.js
@@ -7,13 +7,26 @@ let cartReducer = (state = defaultState, action) => {
     case 'ADD_TO_CART':
       {
         let newState = { ...state }
+        if (action.payload.checkboxValue){
+          console.log('ADD TO CART');
+          
         newState.selectedItems = {
           items: [ ...newState.selectedItems.items, action.payload],
           restaurantName: action.payload.restaurantName
         }
+      } else {
+         console.log('REMOVE FROM CART');
 
-        console.log(newState, ' 👉');
-        return newState;
+         newState.selectedItems = {
+          items: [
+            ...newState.selectedItems.items.filter((item) => item.title != action.payload.title )
+          ],
+          restaurantName: action.payload.restaurantName
+         }
+      }
+
+      console.log(newState, ' 👉');
+      return newState;
       }
 
       default: return state;
