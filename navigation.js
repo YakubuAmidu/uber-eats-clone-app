056f9e8e183e0a6e65ba41112c8de174073138a5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-08-09 20:49:38 -0700

    Initial commit

--- navigation.js
+++ navigation.js
@@ -9,6 +9,7 @@ import configureStore from './redux/store';
 
 import Home from './screens/Home';
 import RestaurantDetail from './screens/RestaurantDetail';
+import OrderCompleted from './screens/OrderCompleted';
 
 const store = configureStore();
 
@@ -24,6 +25,7 @@ export default function RootNavigation(){
     <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+      <Stack.Screen name="OrderCompleted" component={OrderCompleted} />
     </Stack.Navigator>
   </NavigationContainer>
   </ReduxProvider>
